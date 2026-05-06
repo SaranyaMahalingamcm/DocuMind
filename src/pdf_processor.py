268fa6e6289a018356047c0d442a791658b0c86b
saranyamahalingamcm <saranyamahalingam91@gmail.com>
2026-05-05 21:51:52 -0400

    Updated processor.py

--- src/pdf_processor.py
+++ src/pdf_processor.py
@@ -1,43 +1,37 @@
-# # pdf_processor.py
-# # Responsibility: Read a PDF and split it into chunks
-
 from pypdf import PdfReader
 
+# function 1 - to extract the texts from the pdf
 
-def extract_text(uploaded_file):
-    reader = PdfReader(uploaded_file)
-    page_text=[] #create an empty list
+def extract_text(file_uploaded):
+    reader = PdfReader(file_uploaded)
+    pages= []
+    for page in reader.pages:
+        text=page.extract_text()
+        if text != '':
+            pages.append(text)
+        
+    return "\n".join(pages)
 
-    for page in reader.pages(): # loop the variable through each pages extracted by the reader
-        text=page.extract_text() # extract all the texts in the page using the inbuild function and save in the variable text
+#function 2
 
-        if text != "":
-            page_text.append(text) # If the text is not empty, append the values in text to page_text list   
-    all_texts="\n".join(page_text)
-
-    return all_texts
-
-
-def text_chunk(text, size=100, overlap=50):
-    chunks = []
+def chunk_text(text, chunk_size=500,overlap=50):
+    chunks=[]
     start=0
     while len(text)>start:
-        end = start+size
-        chuck_text=text[start:end]
-        chuck_text.strip()
-        if chuck_text !="":
-            chunks.append(chuck_text)
+        end=start+chunk_size
+        chunk=text[start:end]
+        if chunk != '':
+            chunks.append(chunk)
         start=end-overlap
     return chunks
 
-def metadata(uploaded_file):
-    reader= PdfReader(uploaded_file)
-    page_count=len(reader.pages)
-    meta=reader.metadata or {}      
-    title=meta.get("/Title","unknown")
-    author=meta.get("/Author","unknown")
+#function 3
+def get_metadata(file_uploaded):
+    file = PdfReader(file_uploaded)
+    page_count = len(file.pages)
+    meta = file.metadata or {}
     return{
-        'pages': page_count,
-        'title': title,
-        'author':author
+        'pages':page_count,
+        'title':meta.get("/Title","unknown"),
+        'author':meta.get("/Author","unknown")
     }
