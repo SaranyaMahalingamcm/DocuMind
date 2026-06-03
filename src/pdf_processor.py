18bfb66e42832255f23a46627f03eb95ba083260
saranyamahalingamcm <saranyamahalingam91@gmail.com>
2026-06-03 09:40:13 -0400

    updated processor.py

--- src/pdf_processor.py
+++ src/pdf_processor.py
@@ -2,36 +2,39 @@ from pypdf import PdfReader
 
 # function 1 - to extract the texts from the pdf
 
-def extract_text(file_uploaded):
-    reader = PdfReader(file_uploaded)
-    pages= []
+def text_extract(uploaded_file):
+    reader = PdfReader(uploaded_file) # reader object
+    pages = []
     for page in reader.pages:
-        text=page.extract_text()
-        if text != '':
-            pages.append(text)
-        
+        extracted_texts = page.extract_text()
+        if extracted_texts != '':
+            pages.append(extracted_texts)
     return "\n".join(pages)
 
-#function 2
-
+# Function 2- to extract chunk
 def chunk_text(text, chunk_size=500,overlap=50):
     chunks=[]
-    start=0
+    start = 0
     while len(text)>start:
         end=start+chunk_size
         chunk=text[start:end]
-        if chunk != '':
+        chunk=chunk.strip()
+        if chunk != "":
             chunks.append(chunk)
-        start=end-overlap
+        start= end-overlap
     return chunks
 
-#function 3
-def get_metadata(file_uploaded):
-    file = PdfReader(file_uploaded)
-    page_count = len(file.pages)
-    meta = file.metadata or {}
-    return{
-        'pages':page_count,
-        'title':meta.get("/Title","unknown"),
-        'author':meta.get("/Author","unknown")
+#function 3- to extract metadata
+
+def get_metadata(uploaded_file):
+    reader=PdfReader(uploaded_file)
+    meta=reader.metadata or {}
+    title= meta.get("/Title","Unknown")
+    author= meta.get("/Author","Unknown")
+    return {
+    "pages": len(reader.pages),
+    "title": title,
+    "author": author
     }
+
+
